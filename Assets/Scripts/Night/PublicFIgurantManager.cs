d94accfdf741ce4fe59e584cabd1aef448b37411
aolivencia96 <toni.olivencia96@gmail.com>
2024-01-28 16:17:58 +0100

    PublicFigurantManager update

--- Assets/Scripts/Night/PublicFIgurantManager.cs
+++ Assets/Scripts/Night/PublicFIgurantManager.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Managers;
+using Night;
 
 public class PublicFIgurantManager : MonoBehaviour
 {
+    public GameSettings gameSettings;
     public GameObject PublicFigurant_ph;
-    public string[] NpcType;
+    public GameObject figurantPrefab;
     // Start is called before the first frame update
     void Start()
     {
+        NightPreview result = Managers.RoundInitializer.GenerateRound(0, gameSettings);
+        int resultIndex = 0;
         foreach(Transform placeHolder in PublicFigurant_ph.transform){
-            
+            GameObject figurant = Instantiate(figurantPrefab,placeHolder);
+            SpriteRenderer f_SpriteRenderer = figurant.GetComponent<SpriteRenderer>();
+            f_SpriteRenderer.sprite = result.audience[resultIndex].person;
+            resultIndex++;
         }
     }
 
