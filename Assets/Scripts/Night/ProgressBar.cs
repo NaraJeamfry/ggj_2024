84fb2ed4bcd4a7a4b3de15630f9c2f3dadf787ad
Guille <guipradi5@gmail.com>
2024-01-28 13:28:16 +0100

    Añadir progress bar a Night Scene

---
+++ Assets/Scripts/Night/ProgressBar.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+    [Range(0,1  )]
+    public float progress = 0;
+    public GameObject bar;
+    public float barMaxWidthPx = 563;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RectTransform barRectTransform = bar.GetComponent<RectTransform>();
+        float barWidth = progress * barMaxWidthPx;
+        barRectTransform.sizeDelta = new Vector2(barWidth, barRectTransform.sizeDelta.y);
+    }
+    public void updateProgress(float value) {
+        progress = value;
+    }
+}
