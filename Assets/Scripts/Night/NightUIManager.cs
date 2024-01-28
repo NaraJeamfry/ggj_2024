09449594358ca8ddd870c759ddab89350087165f
Marc López Baz <marc@bling.eu>
2024-01-28 16:22:57 +0100

    feat: Merge UI changes from Guille

--- Assets/Scripts/Night/NightUIManager.cs
+++ Assets/Scripts/Night/NightUIManager.cs
@@ -12,6 +12,9 @@ namespace Night
 
         public NightManager nightManager;
 
+        public ProgressBar progress;
+        public ShowImageController show;
+
         private void Start()
         {
             nightManager = GetComponent<NightManager>();
@@ -20,12 +23,15 @@ namespace Night
         private void Update()
         {
             percent.text = $"Progress: {nightManager.NightProgress * 100}";
+            progress.updateProgress(nightManager.NightProgress);
+            
             if (nightManager.waitingChoice)
             {
                 activityName.text = "Waiting...";
             }
             else
             {
+                show.changeActivityType(nightManager.currentActivity);
                 activityName.text = $"Ongoing: {nightManager.currentActivity.activityName}";
             }
         }
