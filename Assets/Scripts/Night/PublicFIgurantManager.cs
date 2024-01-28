b5831e5b6c8494547c252c2ac772f4e377b3e5a5
aolivencia96 <toni.olivencia96@gmail.com>
2024-01-28 11:41:38 +0100

    PublicFigurant prefab v2 + PublicFigurant placeholders

---
+++ Assets/Scripts/Night/PublicFIgurantManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicFIgurantManager : MonoBehaviour
+{
+    public GameObject PublicFigurant_ph;
+    public string[] NpcType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform placeHolder in PublicFigurant_ph.transform){
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
